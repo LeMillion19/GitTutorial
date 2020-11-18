b75b5007a1526446eb411fed330c8dada6af2730
Hassan <hassanakily@gmail.com>
2020-11-18 13:28:10 +0100

    added another class
    counts the sum of two ints

--- src/myClass.java
+++ src/myClass.java
@@ -4,5 +4,11 @@ public class MyClass{
         System.out.println("hej");
     }
 
-    
+    public void AnotherClass{
+        int num1 = 1;
+        int num2 = 19;
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
 }
